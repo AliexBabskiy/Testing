7ea2f3249e806b89b01c6ab3789d3fc1f56b5e06
AliexBabskiy <144725679+AliexBabskiy@users.noreply.github.com>
2024-09-23 00:56:41 +0300

    Add files via upload

---
+++ Password.Tests/PassTest.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Password.Tests
+{
+    [TestClass]
+    public class PassTest
+    {
+        [TestMethod]
+        public void CheckPassword_CheckNumber_1score()
+        {
+            // arrange
+            string password = "1567";
+            var pass = new Password.Pass();
+            int expected = 1;
+            // act
+            int result = pass.CheckPasswordNumber(password);
+            // assert
+            Assert.AreEqual(expected, result);
+            Console.WriteLine("1 балл");
+        }
+
+        [TestMethod]
+        public void CheckPassword_CheckLower_1score()
+        {
+            // arrange
+            string password = "effef";
+            var pass = new Password.Pass();
+            int expected = 1;
+            // act
+            int result = pass.CheckPasswordLower(password);
+            // assert
+            Assert.AreEqual(expected, result);
+            Console.WriteLine("1 балл");
+        }
+
+        [TestMethod]
+        public void CheckPassword_CheckUpper_1score()
+        {
+            // arrange
+            string password = "FEfjdf";
+            var pass = new Password.Pass();
+            int expected = 1;
+            // act
+            int result = pass.CheckPasswordUpper(password);
+            // assert
+            Assert.AreEqual(expected, result);
+            Console.WriteLine("1 балл");
+        }
+
+        [TestMethod]
+        public void CheckPassword_CheckSpecialSymbol_1score()
+        {
+            // arrange
+            string password = "gf@dse_";
+            var pass = new Password.Pass();
+            int expected = 1;
+            // act
+            int result = pass.CheckPasswordSymbol(password);
+            // assert
+            Assert.AreEqual(expected, result);
+            Console.WriteLine("1 балл");
+        }
+
+        [TestMethod]
+        public void CheckPassword_CheckLength_1score()
+        {
+            // arrange
+            string password = "gfdtbydseyh";
+            var pass = new Password.Pass();
+            int expected = 1;
+            // act
+            int result = pass.CheckPasswordLength(password);
+            // assert
+            Assert.AreEqual(expected, result);
+            Console.WriteLine("1 балл");
+        }
+
+        [TestMethod]
+        public void CheckPassword_CheckAll_5score()
+        {
+            // arrange
+            string password = "f2fF1@g_fyk";
+            var pass = new Password.Pass();
+            int expected = 5;
+            // act
+            int result = pass.CheckPasswordNumber(password) + pass.CheckPasswordLower(password) + pass.CheckPasswordUpper(password) + pass.CheckPasswordSymbol(password) + pass.CheckPasswordLength(password);
+            // assert
+            Assert.AreEqual(expected, result);
+            Console.WriteLine("5 балл");
+        }
+    }
+}
---
+++ Password.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Password.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Password.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29b9654a-0ffc-40e9-91ee-d65782aeddaa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
